f49d54f8633000729e47def33a3fc2cceaa26ed7
Erfan <143827987+erfjab@users.noreply.github.com>
2025-10-27 17:59:52 +0330

    improve[usages]: more detail in stats (#15)

--- guardcoreapi/types/stats.py
+++ guardcoreapi/types/stats.py
@@ -1,12 +1,19 @@
+from datetime import datetime
 from pydantic import BaseModel
+
+
+class UsageStats(BaseModel):
+    start: datetime
+    end: datetime
+    usage: int
 
 
 class StatsResponse(BaseModel):
     total_subscription: int
     active_subscription: int
     inactive_subscription: int
-
-    hour_usages: int
-    today_usages: int
-    yesterday_usages: int
-    total_usages: int
+    last_24_hours: list[UsageStats]
+    today_usage: int
+    last_7_days: list[UsageStats]
+    week_usage: int
+    total_usage: int
