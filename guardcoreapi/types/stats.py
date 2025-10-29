f3b3b3d77c179fd2806aaff097ebf298e46b3e52
Erfan <143827987+erfjab@users.noreply.github.com>
2025-10-29 17:58:39 +0330

    improve[stats]: add nodes and admins stats (#16)

--- guardcoreapi/types/stats.py
+++ guardcoreapi/types/stats.py
@@ -2,18 +2,33 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class UsageStats(BaseModel):
-    start: datetime
-    end: datetime
+class UsageDetailStats(BaseModel):
+    start: datetime | None = None
+    end: datetime | None = None
+    remark: str | None = None
     usage: int
 
 
 class StatsResponse(BaseModel):
-    total_subscription: int
-    active_subscription: int
-    inactive_subscription: int
-    last_24_hours: list[UsageStats]
-    today_usage: int
-    last_7_days: list[UsageStats]
-    week_usage: int
-    total_usage: int
+    total_subscriptions: int
+    active_subscriptions: int
+    inactive_subscriptions: int
+    online_subscriptions: int
+    most_usage_subscription: str | None = None
+    most_usage_subscriptions: list[UsageDetailStats]
+
+    total_admins: int
+    active_admins: int
+    inactive_admins: int
+    most_usage_admins: list[UsageDetailStats]
+
+    total_nodes: int
+    active_nodes: int
+    inactive_nodes: int
+    most_usage_nodes: list[UsageDetailStats]
+
+    total_lifetime_usages: int
+    total_day_usages: int
+    total_week_usages: list[UsageDetailStats]
+    last_24h_usages: list[UsageDetailStats]
+    last_7d_usages: list[UsageDetailStats]
